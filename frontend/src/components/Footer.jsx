ccb448a5afc7b4616ff1ab090bf3978e3302afca
Mihai Andrei <mihai.andrei1597@gmail.com>
2022-08-20 19:19:57 +0300

    added show layout

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,19 +1,19 @@
 export default function Footer() {
     return (<>
         <footer
-            className="flex justify-center -mt-12 container bg-transparent dark:bg-gray-900 dark:text-dark-accent text-gray-600 py-3 px-10 mx-auto max-w-3xl lg:max-w-5xl -mb-4 rounded-t-lg text-center"
+            className="text-gray-100 flex justify-center -mt-12 container bg-gradient-to-r from-fuchsia-800 via-purple-900 to-pink-400 py-3 px-10 mx-auto max-w-3xl lg:max-w-5xl -mb-4 rounded-t-lg text-center"
         >
             <p>
                 ©2022 Made with ❤ by {" "}
                 <a
-                    className="font-bold"
+                    className="font-bold text-yellow-300"
                     target="_blank"
                     href="https://twitter.com/MihaiAdrianAnd1"
                 >Andrei Mihai</a
                 >
                 <span> and </span>
                 <a
-                    className="font-bold"
+                    className="font-bold text-yellow-300"
                     target="_blank"
                     href="https://twitter.com/MihaiAdrianAnd1"
                 >Lapusneanu Maria Cristina</a
--- frontend/src/pages/Show.jsx
+++ frontend/src/pages/Show.jsx
@@ -4,7 +4,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Loader from "../components/Loader";
 import { useAuth } from "../contexts/Auth";
 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w780'
 
 const fetchShowInfo = async ({ type, id }) => {
 
@@ -32,7 +32,6 @@ export default function Show() {
         onSuccess: () => {
             // Invalidate and refetch
             queryClient.invalidateQueries(["showData", { type, id }])
-            console.log('intra aici');
         },
 
     })
@@ -54,46 +53,44 @@ export default function Show() {
 
     if (isLoading) return <Loader />;
 
-    if (error) return "An error has occurred: " + error.message;
-
+    if (error) return "An error has occurred: " + error?.message;
 
     return (
-        <>
-            {data.supabase_response ? 'da' : 'nu'}
-            <div className="flex flex-col md:flex-row">
-                <div className="order-2 md:order-1 w-full md:w-3/4 pb-6">
-                    <div className="mb-5">
-                        <h5 className="text-gray-200 dark:text-dark-accent font-bold text-2xl tracking-tight mb-4">
-                            {type === 'series' ? data.tmdb_response.name : data.tmdb_response.title}
-                        </h5>
-                        <p className="text-gray-100 font-light italic" >
-                            {data.tmdb_response.genres.map(item => item.name).join(' - ')}
-                        </p>
-                    </div>
-                    <div className="mt-4 text-justify dark:text-gray-200">
-                        <small>{data.tmdb_response.overview}</small>
-                    </div>
-                </div>
-                <div className="order-1 md:order-2 w-full md:w-1/4">
-                    <img
-                        className="
-                    max-h-full
-                    w-auto
-                    mx-auto
-                    object-cover
-                    rounded-t-lg
-                    md:rounded-none md:rounded-l-lg
-                    shadow-xl
-                    mb-4 md:mb-0
-                    "
-                        src={IMG_PATH + data.tmdb_response.poster_path}
-                        alt=""
-                        style={{ maxWidth: 150 }}
-                    />
-                </div>
+        <div className="max-w-[780px] mx-auto">
 
+            <h5 className="text-gray-200 dark:text-dark-accent font-bold text-3xl tracking-tight mb-4 text-center">
+                {type === 'series' ? data.tmdb_response.name : data.tmdb_response.title}
+            </h5>
+
+
+            <div className="h-[300px]  mx-auto w-auto rounded-2xl overflow-hidden relative">
+                {data.tmdb_response.vote_average ? <div className="absolute top-0 right-0 p-4 bg-yellow-300 text-gray-900 text-md font-bold">
+                    {data.tmdb_response.vote_average.toFixed(2)}
+                </div> : ''}
+                <img className="w-full h-full" src={IMG_PATH + data.tmdb_response.backdrop_path} alt="" />
             </div>
-            <button className="p-4 bg-yellow-300" onClick={addMovieToDb}>Add movie to db</button>
-        </>
+            <div className="flex gap-2 mt-4">
+                <span className="text-yellow-400 w-32 block font-bold">Genre:</span>
+                <p className="text-gray-100 font-light italic w-full" >
+                    {data.tmdb_response.genres.map(item => item.name).join(' - ')}
+                </p>
+            </div>
+            <div className="flex gap-2 mt-4 text-justify">
+                <span className="text-yellow-400 w-32 block font-bold">Description:</span>
+                <p className="text-gray-100 font-light italic w-full" >
+                    {data.tmdb_response.overview}
+                </p>
+            </div>
+            {data.tmdb_response?.release_date && <div className="flex gap-2 mt-4 text-justify">
+                <span className="text-yellow-400 w-32 block font-bold">Release Date:</span>
+                <p className="text-gray-100 font-light italic w-full" >
+                    {data.tmdb_response.release_date}
+                </p>
+            </div>
+            }
+
+            {data.supabase_response ? '' : <button className="p-4 bg-yellow-300" onClick={addMovieToDb}>Add movie to db</button>}
+
+        </div>
     )
 }
